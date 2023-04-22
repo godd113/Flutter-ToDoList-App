a380158643e27db6486e32ce8321c403e82476c6
titletp <siripapar@efinancethai.com>
2023-04-22 19:14:16 +0700

    first commit

---
+++ lib/modules/module_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class ModuleColors {
+  static Color themeColor = const Color.fromRGBO(
+    131,
+    33,
+    255,
+    1.0,
+  );
+}
---
+++ lib/pages/launch_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:todo/modules/module_colors.dart';
+
+class LaunchScreen extends StatefulWidget {
+  const LaunchScreen({super.key});
+
+  @override
+  State<LaunchScreen> createState() => _LaunchScreenState();
+}
+
+class _LaunchScreenState extends State<LaunchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Container(
+            decoration: const BoxDecoration(color: Colors.transparent),
+            height: 500, // specify the height of the container
+            child: Column(children: [
+              Center(
+                child: Image.asset(
+                  'assets/images/vectors/vector_todo.png',
+                  height: 330,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: DefaultTextStyle(
+                  style: TextStyle(
+                      color: ModuleColors.themeColor,
+                      fontSize: 50,
+                      fontStyle: FontStyle.normal,
+                      fontFamily: 'Kanit'),
+                  child: const Text('To Do'),
+                ),
+              )
+            ])),
+      ),
+    );
+  }
+}
